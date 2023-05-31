d55bd55ad63ca6edf6ee6bfc9368ea2ec43dd8d9
afafadia <133740809+afafadia@users.noreply.github.com>
2023-05-31 23:42:01 +0530

    Using click library

--- openlib.py
+++ openlib.py
@@ -1,65 +1,92 @@
-import json
-
+import click
 import requests
 
-menu_options = {
-    1: "Search Books by Author",
-    2: "Option 2",
-    3: "Option 3",
-    4: "Option 4",
-    5: "Exit",
-}
 
+@click.command()
+def main():
+    choice = None
 
-def print_menu():
-    import os
+    while choice != "q":
+        click.clear()
+        click.echo("Menu:")
+        click.echo("1. Search books by Works API")
+        click.echo("2. Search books by Editions API")
+        click.echo("3. Search books by ISBN API")
+        click.echo("4. Search books by Book API - query params: `bibkeys`")
+        click.echo("q. Quit")
 
-    os.system("clear")
-    for key in menu_options.keys():
-        print(key, "--", menu_options[key])
+        choice = click.prompt("\nEnter your choice")
+
+        if choice == "1":
+            option1()
+        elif choice == "2":
+            option2()
+        elif choice == "3":
+            option3()
+        elif choice == "4":
+            option4()
 
 
 def option1():
-    print("\nSearch Books by Author.....e.g. Ani Mestre")
-    resp = requests.get("http://openlibrary.org/search.json?author=Ani+Mestre")
-    print(json.dumps(resp.json(), indent=2))
-    input("\n\nPress Enter to continue...")
+    """
+    Search books by Works API
+    """
+    print("\nSearch books by Works API.....e.g. 'OL45804W', 'OL7353617M' etc...")
+    id = click.prompt("Enter ID")
+    try:
+        response = requests.get(f"https://openlibrary.org/works/{id}.json")
+        print("\n", response.json())
+    except requests.exceptions.ConnectionError as e:
+        print(f"\n\nError trying to connect to Openlibrary API: {e}")
+    input("\n\nPress any key to continue...")
 
 
 def option2():
-    print("Handle option 'Option 2'")
-    input("\n\nPress Enter to continue...")
+    """
+    Search books by Editions API
+    """
+    print("\nSearch books by Editions API.....e.g. 'OL45804W', 'OL7353617M' etc...")
+    id = click.prompt("Enter ID")
+    try:
+        response = requests.get(f"https://openlibrary.org/books/{id}.json")
+        print("\n", response.json())
+    except requests.exceptions.ConnectionError as e:
+        print(f"\n\nError trying to connect to Openlibrary API: {e}")
+    input("\n\nPress any key to continue...")
 
 
 def option3():
-    print("Handle option 'Option 3'")
-    input("\n\nPress Enter to continue...")
+    """
+    Search books by ISBN API
+    """
+    print("\nSearch books by ISBN.....e.g. '9780140328721', '9788449433993', '1405933259' etc...")
+    isbn = click.prompt("Enter ISBN No")
+
+    try:
+        response = requests.get(f"https://openlibrary.org/isbn/{isbn}.json/")
+        print("\n", response.json())
+    except requests.exceptions.ConnectionError as e:
+        print(f"\n\nError trying to connect to Openlibrary API: {e}")
+    input("\n\nPress any key to continue...")
 
 
 def option4():
-    print("Handle option 'Option 4'")
-    input("\n\nPress Enter to continue...")
+    """
+    Search books by Books API - query params: `bibkeys`
+    """
+    print(
+        "\nSearch books by BookAPI.....e.g. 'ISBN:0201558025', 'OCLC:1003656209', 'LCCN:93005405', 'OLID:OL8492069M', 'ISBN:0385472579,LCCN:62019420' etc..."
+    )
+    bibkeys = click.prompt("Enter bibkeys")
+    params = {"bibkeys": bibkeys, "format": "json"}
+
+    try:
+        response = requests.get("https://openlibrary.org/api/books", params=params)
+        print("\n", response.json())
+    except requests.exceptions.ConnectionError as e:
+        print(f"\n\nError trying to connect to Openlibrary API: {e}")
+    input("\n\nPress any key to continue...")
 
 
 if __name__ == "__main__":
-    while True:
-        print_menu()
-        option = ""
-        try:
-            option = int(input("Enter your choice: "))
-        except:
-            input("\n\nWrong input. Numeric inputs only...")
-        # Check what choice was entered and act accordingly
-        if option == 1:
-            option1()
-        elif option == 2:
-            option2()
-        elif option == 3:
-            option3()
-        elif option == 4:
-            option4()
-        elif option == 5:
-            print("\nThanks message before exiting\n\n")
-            exit()
-        else:
-            input("\nInvalid option. Please enter a number between 1 and 5.\n\n")
+    main()
